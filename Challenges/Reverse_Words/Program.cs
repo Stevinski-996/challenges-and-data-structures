3165c96ae49683a0fa159076e39e5ad1e07a0920
stevinski996 <23041521@student.ltuc.com>
2024-07-15 22:56:26 +0300

    Reverse-Words

---
+++ Challenges/Reverse_Words/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+
+class Program {
+    public static void Main (string[] args) 
+    {
+        Console.WriteLine("Enter your text here :");
+        string str = Console.ReadLine();
+        string output = ReversWord(str);
+        Console.WriteLine(output);
+
+    }
+static string ReversWord(string word){
+    string[] words = word.Split(' ');
+    string[] rw = new string[words.Length];
+    for(int i = 0; i < words.Length ; i++) {
+        rw[i] = words[words.Length-(i+1)];
+    }
+    return string.Join(" ", rw);    ;
+}
+
+    }
