8a8e21241bec7ea1150aa28cef108b499d98ac32
stevinski996 <23041521@student.ltuc.com>
2024-07-15 21:45:40 +0300

    Updating Code

--- Challenges/Find-Duplicates/FindDuplicatesApp/Program.cs
+++ Challenges/Find-Duplicates/FindDuplicatesApp/Program.cs
@@ -8,13 +8,17 @@ namespace FindDuplicatesApp
         static void Main(string[] args)
         {
             int[] input = { 1, 2, 3, 1, 2, 3 };
+            int[] input2 = { 16, 8, 31, 17, 15, 23, 17 ,8};
+            int[] input3 = { 5, 10, 16, 20, 10, 16};
             List<int> output = FindDuplicates(input);
-
-            Console.WriteLine("Output Array: ");
-            foreach (var item in output)
-            {
-                Console.Write(item + " ");
-            }
+            List<int> output2 = FindDuplicates(input2);
+            List<int> output3 = FindDuplicates(input3);
+            Console.WriteLine("\nDuplicated Output1 Array: ");
+            printValues(output);
+            Console.WriteLine("\nDuplicated Output2 Array: ");
+            printValues(output2);
+            Console.WriteLine("\nDuplicated Output3 Array: ");
+            printValues(output3);
         }
 
         public static List<int> FindDuplicates(int[] input)
@@ -34,6 +38,13 @@ namespace FindDuplicatesApp
             }
 
             return duplicates;
+        }
+        public static void printValues(List<int> output) {
+
+            foreach (var item in output)
+            {
+                Console.Write(item + " ");
+            }
         }
     }
 }
