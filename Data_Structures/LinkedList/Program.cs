9076d94a44e2a6d9bb6e43e2fffa5694a5673792
stevinski996 <23041521@student.ltuc.com>
2024-07-21 01:06:28 +0300

    LinkedList

---
+++ Data_Structures/LinkedList/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace  LinkedList {
+
+    public class Program {
+
+        public static void Main(string[] args) {
+            LinkList list = new LinkList();
+            int[] Case1={
+                  5,
+                  10,
+                  20,
+                  30
+            };
+                foreach (var link in Case1){
+                list.Add(link);
+            }
+                // AddToList(list,Case1);  
+                list.Remove(10);
+                var results = list.Includes(10);
+                Console.WriteLine(results);
+                list.Print();
+                      }
+        static void AddToList(LinkList list, int[] Output){
+            foreach (var link in Output){
+                list.Add(link);
+            }
+        }
+    }
+}
